6c3c342992d0a21dcce0f7665f34e344c6368e0b
mta630 <mta630@ddubb.net>
2025-06-10 16:04:49 -0500

    update config with proper name

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This is the repository home for my personal sandbox website mta630.com
 
 ## Dragonball gifs for no reason
+
 ![goten_10](https://github.com/user-attachments/assets/1cd55d58-d847-4f66-9f9c-47f13814fc46)
 ![piccolo_17](https://github.com/user-attachments/assets/5a5b1578-0a4a-4155-a2bb-de4b5383d703)
 ![great_saiyaman_4](https://github.com/user-attachments/assets/fe4eaf15-762b-4306-99fd-a98a817c2c66)
--- apps/www/src/config/site.ts
+++ apps/www/src/config/site.ts
@@ -1,18 +1,10 @@
 export const siteConfig = {
-  name: "GuildForge",
+  name: "mta630",
   url: "https://www.mta630.com",
   ogImage: "https://www.mta630.com/og.jpg",
   description: "mta630's website. lotta random stuff, have fun!",
   links: {},
-  nav: [
-    { name: "Home", href: "/" },
-    { name: "Calculator", href: "/calculator" },
-  ],
+  nav: [{ name: "Home", href: "/" }],
 };
 
 export type SiteConfig = typeof siteConfig;
-
-export const META_THEME_COLORS = {
-  light: "#ffffff",
-  dark: "#09090b",
-};
